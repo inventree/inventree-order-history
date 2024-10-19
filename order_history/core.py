3ed7066b007716aef89769d46cbc746d53ba526a
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-19 03:06:31 +0000

    Generate placeholder panel

--- order_history/core.py
+++ order_history/core.py
@@ -22,12 +22,50 @@ class OrderHistoryPlugin(SettingsMixin, UrlsMixin, UserInterfaceMixin, InvenTree
     TITLE = "Order History Plugin"
 
     SETTINGS = {
-        # TODO: Fill out settings
+        'BUILD_ORDER_HISTORY': {
+            'name': 'Build Order History',
+            'description': 'Enable build order history tracking',
+            'default': True,
+            'validator': bool,
+        },
+        'PURCHASE_ORDER_HISTORY': {
+            'name': 'Purchase Order History',
+            'description': 'Enable purchase order history tracking',
+            'default': True,
+            'validator': bool,
+        },
+        'SALES_ORDER_HISTORY': {
+            'name': 'Sales Order History',
+            'description': 'Enable sales order history tracking',
+            'default': True,
+            'validator': bool,
+        },
+        'RETURN_ORDER_HISTORY': {
+            'name': 'Return Order History',
+            'description': 'Enable return order history tracking',
+            'default': True,
+            'validator': bool,
+        },
     }
 
     def get_ui_panels(self, request, context=None, **kwargs):
         """Return a list of UI panels to be rendered in the InvenTree user interface."""
 
-        print("get_ui_panels:", context)
+        target = context.get('target_model')
+
+        if target == 'part':
+            return [
+                {
+                    'key': 'order-history',
+                    'title': 'Order History',
+                    'description': 'View order history for this part',
+                    'source': self.plugin_static_file(
+                        'OrderHistoryPanel.js:renderPanel'
+                    ),
+                    'context': {
+                        'settings': self.get_settings_dict(),
+                    }
+                }
+            ]
 
         return []
