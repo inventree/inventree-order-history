937fe053bd3e66e58266c9d6b7ef8b1ccafc4086
Oliver Walters <oliver.henry.walters@gmail.com>
2025-04-07 23:22:55 +0000

    Update frontend packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    viteExternalsPlugin({
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@mantine/core': 'MantineCore',
+      "@mantine/notifications": 'MantineNotifications',
+    }),
+  ],
   build: {
     cssCodeSplit: false,
     manifest: false,
@@ -16,7 +27,17 @@ export default defineConfig({
         dir: '../order_history/static',
         entryFileNames: '[name].js',
         assetFileNames: 'assets/[name].[ext]',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@mantine/core': 'MantineCore',
+          "@mantine/notifications": 'MantineNotifications',
+        },
       },
-      }
-  }
+      external: ['react', 'react-dom', '@mantine/core', '@mantine/notifications'],
+    }
+  },
+  optimizeDeps: {
+    exclude: ['react', 'react-dom', '@mantine/core', '@mantine/notifications'],
+  },
 })
